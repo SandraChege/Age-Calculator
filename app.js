76519a25e00be8e49bdfc507c7fb9c21fb89378b
Sandra Chege <chegemwihaki88@gmail.com>
2023-10-15 17:00:33 +0300

    Calculates the age of a person

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 let inputs = document.querySelectorAll("input");
 const errorMessage = document.querySelector(".error-message");
 const btn = document.querySelector(".calc-button")
+const yearResult = document.querySelector("#years");
+const monthResult = document.querySelector("#months");
+const daysResult = document.querySelector("#days");
 // console.log(inputs);
 
-let currentDay = new Date().getDay();
+let currentDay = new Date().getDate();
 let currentMonth = new Date().getMonth();
 let currentYear = new Date().getFullYear();
 
@@ -12,7 +15,7 @@ let day, month, year;
 inputs[0].addEventListener('input', (e) => {
     let valueday = inputs[0].value; //get the value from input field
     if (valueday >= 1 && valueday <= 31) {
-      day = parseInt(valueday);
+        day = parseInt(valueday);
       // console.log(day);
     } else {
       errorMessage.textContent = "Enter a valid day";
@@ -22,7 +25,8 @@ inputs[0].addEventListener('input', (e) => {
 inputs[1].addEventListener("input", (e) => {
   let valuemonth = inputs[1].value; //get the value from input field
   if (valuemonth >= 1 && valuemonth <= 12) {
-      let month = parseInt(valuemonth);
+      month = parseInt(valuemonth) - 1;
+      errorMessage.textContent = "";
     //console.log(month);
   } else {
     errorMessage.textContent = "Enter a valid month";
@@ -33,26 +37,36 @@ inputs[1].addEventListener("input", (e) => {
 inputs[2].addEventListener("input", (e) => {
   let valueyear = inputs[2].value; //get the value from input field
   if (parseInt(valueyear) >= 1000 && parseInt(valueyear) <= currentYear) {
-    let year = parseInt(valueyear);
+    year = parseInt(valueyear);
+    errorMessage.textContent = "";
     //console.log(year);
   } else {
-    errorMessage.textContent = "Enter a valid day";
+    errorMessage.textContent = "Enter a valid year";
   }
 });
 
+btn.addEventListener("click", calculate);
+
 function calculate() {
-    let myDay = currentDay - day;
-    let myMonth = currentMonth - month;
-    let myYear = currentYear - year;
-    if (myDay < 0) {
-      myMonth -= 1;
-      myDay += 30;
+    if (day && month && year !== "" ) {
+        let myDay = currentDay - day;
+        let myMonth = currentMonth - month;
+        let myYear = currentYear - year;
+        if (myDay < 0) {
+          myMonth -= 1;
+          myDay += 30;
+        }
+        if (myMonth < 0) {
+          myYear -= 1;
+          myMonth += 12;
+        }
+        yearResult.textContent = myYear;
+        monthResult.textContent = myMonth;
+        daysResult.textContent = myDay;
+        
+    } else {
+        errorMessage.textContent="Please enter a valid date"
     }
-    if (myMonth < 0) {
-      myYear -= 1;
-      myMonth += 12;
-    }
-    console.log(myYear);
 }
 
 
